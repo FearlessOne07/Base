6c7efc3febef021e66b3e0c35ce3305263f95d89
Baraka Joel <bjtalems@gmail.com>
2025-02-17 20:57:03 +0000

    Added Camera System and component

---
+++ include/base/components/CameraComponent.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "base/Component.hpp"
+#include "raylib.h"
+
+namespace Base
+{
+  enum struct BASEAPI CameraMode
+  {
+    BASIC_FOLLOW = 0,
+    SMOOTH_FOLLOW = 1
+  };
+
+  struct BASEAPI CameraComponent : public Component
+  {
+    Vector2 target = {0, 0};
+    CameraMode cameraMode;
+  };
+} // namespace Base
